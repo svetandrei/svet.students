fd7f573169d586fb97512dc25fbb3fecec73a180
Timofey <timoxa17@gmail.com>
2022-03-01 12:47:48 +0300

    tank tests fix

--- src/tank.test.js
+++ src/tank.test.js
@@ -2,7 +2,7 @@ import moveTank from './tank';
 
 const logSpy = jest.spyOn(console, 'log').mockImplementation((str) => str.toLowerCase().split('ё').join('е'));
 
-describe('tank', () => {
+describe('танк', () => {
   afterEach(() => {
     logSpy.mockClear();
   });
@@ -11,22 +11,24 @@ describe('tank', () => {
     logSpy.mockRestore();
   });
 
-  it('moves all the way without mines', () => {
+  it('двигается на 10 клеток, если нет мин', () => {
     moveTank([false, false, false, false, false, false, false, false, false, false]);
     expect(logSpy).toHaveBeenCalledTimes(10);
     expect(logSpy).toHaveLastReturnedWith('танк переместился на 10');
   });
 
-  it('moves all the way with one mine', () => {
+  it('двигается на 10 клеток, если есть 1 мина', () => {
     moveTank([false, false, false, false, true, false, false, false, false, false]);
-    expect(logSpy).toHaveBeenCalledTimes(10);
+    // 10 выводов о перемещении, 1 о повреждении
+    expect(logSpy).toHaveBeenCalledTimes(11);
     expect(logSpy).toHaveNthReturnedWith(5, 'танк поврежден');
     expect(logSpy).toHaveLastReturnedWith('танк переместился на 10');
   });
 
-  it('moves until second mine has been reached', () => {
+  it('двигается до 2-й мины, затем уничтожается', () => {
     moveTank([false, false, false, false, true, false, true, false, false, false]);
-    expect(logSpy).toHaveBeenCalledTimes(7);
+    // 7 выводов о перемещении, 1 о повреждении, 1 об уничтожении
+    expect(logSpy).toHaveBeenCalledTimes(9);
     expect(logSpy).toHaveNthReturnedWith(5, 'танк поврежден');
     expect(logSpy).toHaveLastReturnedWith('танк уничтожен');
   });
