17ea1929a651b080ec30dac00d82e105d4af1543
Timofey <timoxa17@gmail.com>
2022-03-01 17:04:40 +0300

    copied from frontend-pro/m05-functions

--- src/cart-total.test.js
+++ src/cart-total.test.js
@@ -3,54 +3,53 @@ import calculate from './cart-total';
 const MINUS_300 = 'ДАРИМ300';
 const MINUS_15_PERCENT = 'СКИДКА15';
 
-describe('Cart total calculator', () => {
-  it('should return same amount without any promotions', () => {
+describe('калькулятор суммы корзины', () => {
+  it('сумма корзины равна сумме товаров, если нет никаких скидок', () => {
     expect(calculate(10, 9)).toEqual(10);
     expect(calculate(100, 1)).toEqual(100);
     expect(calculate(1000, 5)).toEqual(1000);
     expect(calculate(49999, 3)).toEqual(49999);
   });
 
-  it(`should properly apply ${MINUS_300} promocode`, () => {
+  it(`промокод ${MINUS_300} применяется корректно`, () => {
     expect(calculate(1000, 1, MINUS_300)).toEqual(700);
     expect(calculate(820, 9, MINUS_300)).toEqual(520);
     expect(calculate(100, 1, MINUS_300)).toEqual(0);
     expect(calculate(100, 15, MINUS_300)).toEqual(0);
   });
 
-  it('should properly apply 5% discount for 10+ units', () => {
+  it('скидка на 5% на 10+ товаров применятеся корректно', () => {
     expect(calculate(100, 10)).toEqual(95);
     expect(calculate(500, 15)).toEqual(475);
     expect(calculate(500, 17, MINUS_300)).toEqual(190);
     expect(calculate(180, 12, MINUS_300)).toEqual(0);
   });
 
-  it('should properly apply 20% discount to 50k+ surplus', () => {
-    // simple case
+  it('скидка 20% корректно применяется на превышение 50k от суммы корзины', () => {
     expect(calculate(50000, 2)).toEqual(50000);
 
-    // when 20% surplus discount should not be applied
+    // ситуации, когда скидка не должна применяться
     expect(calculate(50100, 1, MINUS_300)).toEqual(49800);
     expect(calculate(52300, 30, MINUS_300)).toEqual(49400);
     expect(calculate(52000, 22)).toEqual(49400);
 
-    // when 20% surplus discount should be applied
+    // ситуации, когда скидка должна применяться
     expect(calculate(60300, 1, MINUS_300)).toEqual(58000);
-    // 70 000 - 5% - 20% on surplus
+    // 70 000 - 5% - 20% на превышение
     expect(calculate(70000, 12)).toEqual(63200);
-    // 70 000 - 300 - 5% - 20% on surplus
+    // 70 000 - 300 - 5% - 20% на превышение
     expect(calculate(70300, 12, MINUS_300)).toEqual(63200);
   });
 
-  it(`should properly apply ${MINUS_15_PERCENT} promocode to 20k+ carts`, () => {
+  it(`промокод ${MINUS_15_PERCENT} на скидку 15% на корзину с суммой 20k+ применятеся корректно`, () => {
     expect(calculate(19000, 4, MINUS_15_PERCENT)).toEqual(19000);
     expect(calculate(20000, 2, MINUS_15_PERCENT)).toEqual(17000);
 
-    // 21 000 - 5% < 20k, should not be applied
+    // 21 000 - 5% < 20k, промокод не сработает
     expect(calculate(21000, 12, MINUS_15_PERCENT)).toEqual(19950);
-    // 60 000 - 20% on surplus - 15%
+    // 60 000 - 20% на превышение - 15%
     expect(calculate(60000, 1, MINUS_15_PERCENT)).toEqual(49300);
-    // 60 000 - 5% - 20% on surplus - 15%
+    // 60 000 - 5% - 20% на превышение - 15%
     expect(calculate(60000, 94, MINUS_15_PERCENT)).toEqual(47260);
   });
 });
--- src/filter-black-list.test.js
+++ src/filter-black-list.test.js
@@ -1,7 +1,7 @@
 import filter from './filter-black-list';
 
-describe('Black list strings filter function', () => {
-  it('should just work as expected', () => {
+describe('фильтр массива строк по чёрному списку', () => {
+  it('фильтр работает корректно', () => {
     expect(filter(['1', '2', '3', '4', '5'], ['3', '4']).sort()).toEqual(['1', '2', '5']);
     expect(filter(['1', '2', '3', '4', '5'], ['0', '3', '4']).sort()).toEqual(['1', '2', '5']);
     expect(filter(['1', '2', '3', '4', '5'], ['6', '7', '8']).sort()).toEqual(['1', '2', '3', '4', '5']);
