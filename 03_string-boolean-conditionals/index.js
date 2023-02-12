81fda3ed6411d12be623a1b26b838c23d07597ed
Andrei <svetsandrew@gmail.com>
2023-02-12 15:47:57 +0200

    JS - fix lesson of module 3

--- 03_string-boolean-conditionals/index.js
+++ 03_string-boolean-conditionals/index.js
@@ -4,7 +4,7 @@
 
 let password = '4321_';
 
-if (password >= 4 && password.includes('_')) {
+if (password >= 4 && (password.includes('_') || password.includes('-'))) {
   console.log('Пароль надёжный');
 } else {
   console.log('Пароль недостаточно надёжный');
@@ -17,10 +17,18 @@ if (password >= 4 && password.includes('_')) {
 let userName = 'аНдРЕй';
 let userSurname = 'шВеЦ';
 
-let lower = userName.toLowerCase();
-let firstLetter = lower.slice(0,1);
-let resCapitalized = lower.replace(firstLetter, firstLetter.toUpperCase());
-console.log(resCapitalized);
+transformation = function (str, name = '') {
+  let lower = str.toLowerCase();
+  let firstLetter = lower.slice(0, 1);
+  let resCapitalized = lower.replace(firstLetter, firstLetter.toUpperCase());
+  if (str === resCapitalized) {
+    console.log(name, 'осталось без изменений');
+  } else {
+    console.log(name, 'было преобразовано');
+    console.log(resCapitalized);
+  }
+}
+transformation(userSurname, 'Фамилия');
 
 /**
  * Задача №3
