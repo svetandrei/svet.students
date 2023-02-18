a1b9dab5ec11146418a24d3db871baceb9bf2b8d
Andrei <svetsandrew@gmail.com>
2023-02-18 19:06:49 +0200

    Fix task 3, task 4, JS - Lesson 4

--- src/task_3.js
+++ src/task_3.js
@@ -5,10 +5,10 @@ for (let i = 0; i < arrRoads.length; i++) {
   let position = i + 1;
 
   if (arrRoads[i] === true && hit === false) {
-    console.log(`Танк поврежден`);
+    console.log('Танк поврежден');
     hit = true;
   } else if (arrRoads[i] === true && hit === true) {
-    console.log(`Танк уничтожен`);
+    console.log('Танк уничтожен');
     break;
   } else {
     console.log(`Танк переместился на ${position}`);
